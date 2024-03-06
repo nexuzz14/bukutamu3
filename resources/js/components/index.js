0f9922b5e15cde7ffd9ed5d1536471a728f14c24
nexuzz <125716518+nexuzz14@users.noreply.github.com>
2024-03-06 08:56:55 +0700

    Update index.js

--- resources/js/components/index.js
+++ resources/js/components/index.js
@@ -18,7 +18,8 @@ export default function Index() {
                     <p className="text-sm lg:font-normal text-gray-700 dark:text-gray-400">135, Jl. Ahmad Yani, South Kramat</p>
                     <p className="text-sm lg:font-normal text-gray-700 dark:text-gray-400">Magelang Utara, Magelang City</p>
                     <p className="text-sm lg:font-normal mb-7 text-gray-700 dark:text-gray-400">Central Java 59155</p>
-                    <a href="#pendaftaran" className="text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"><i className="mdi mdi-text-box-edit-outline"></i> Daftar</a>
+                    {window.userData.status=="verified"?<a href="#scan" className="text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"><i className="mdi mdi-qrcode-scan"></i> Scan</a>:
+                    <a href="#pendaftaran" className="text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"><i className="mdi mdi-text-box-edit-outline"></i> Pendaftaran</a>}
                     <a href="#tutorial" className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"><i className="mdi mdi-book-open-variant"></i> Tutorial</a>
                 </div>
                 <div>
